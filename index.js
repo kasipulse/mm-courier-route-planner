9ff1e8d15cfe8e0e1eaca6b0526b453f56c1387b
kasipulse <148783028+kasipulse@users.noreply.github.com>
2025-10-14 14:25:47 +0200

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+import express from "express";
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Route planner API is running 🚀");
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
