d7621a31259d5f280e9c04fc08cd17f38358f08b
kasipulse <148783028+kasipulse@users.noreply.github.com>
2025-10-14 14:35:07 +0200

    index.js

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 import express from "express";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
-  res.send("Route planner API is running 🚀");
+  res.json({ message: "Route Planner API is running 🚀" });
 });
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`✅ Server running on port ${PORT}`);
+});
